3c2a3686cc6423439a12fe446b87c0cefb8db149
chnmk <a2molchanov@ya.ru>
2024-01-15 13:26:53 +0300

    fix: add favicon to useHead in app.vue script

--- app.vue
+++ app.vue
@@ -11,6 +11,7 @@ useHead({
   title: 'typelearner',
   meta: [
     { name: 'description', content: 'Yet another language learning app.' }
-  ]
+  ],
+  link: [{ rel: 'icon', type: 'image/png', href: 'favicon.ico' }]
 })
 </script>
