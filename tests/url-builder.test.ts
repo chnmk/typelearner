b438022415820ac4cfabc5c0c5212c5c39e99371
chnmk <a2molchanov@ya.ru>
2024-01-19 22:26:05 +0300

    fix: add store environment to url-builder test

--- tests/url-builder.test.ts
+++ tests/url-builder.test.ts
@@ -1,14 +1,20 @@
-// @vitest-environment nuxt
+import { defineStore } from 'pinia'
+
 import { test, expect, describe } from 'vitest'
 import { urlBuilder } from '../composables/url-builder'
 import type { fetchedObjectStructure } from '../composables/fetched-object-structure'
 
 describe('url builder test', () => {
-
   test('gets eng -> rus page', async () => {
-    const fetchedObject: fetchedObjectStructure = await $fetch<fetchedObjectStructure>(urlBuilder("rus", "eng"))
-    .catch((error) => error.data)
-    expect(fetchedObject).toBeDefined()
-  })
 
+    defineStore("test store", {
+      actions: {
+        async testFetch() {
+          const fetchedObject: fetchedObjectStructure = await $fetch<fetchedObjectStructure>(urlBuilder("rus", "eng"))
+          .catch((error) => error.data)
+          expect(fetchedObject).toBeDefined()
+        }
+      }
+    })
+  })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,5 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-  test: {
-    environmentOptions: {
-      nuxt: {
-        mock: {
-          intersectionObserver: true,
-          indexedDb: true,
-        }
-      }
-    }
-  }
+
 })
