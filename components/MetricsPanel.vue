31705e9afbda6d1bc8af9c6eb59a9d89260ee237
chnmk <a2molchanov@ya.ru>
2024-01-20 17:21:36 +0300

    style: add v-card components to metrics panel

--- components/MetricsPanel.vue
+++ components/MetricsPanel.vue
@@ -8,46 +8,47 @@
     <p class="sheet-header">Metrics</p>
     <hr class="header-hr" />
     <div class="metrics-tab">
-      <div class="metrics-div">
-        <span class="counter-left">Timer: </span>
-        <span class="counter-right">
-          <span class="metrics-numbers">{{ metricsStore.time }}</span>
-          <span class="m-space"> s</span>
-        </span>
-      </div>
+      <v-card class="metrics-card" hover>
+        <div class="metrics-div">
+          <span class="counter-left">Timer: </span>
+          <span class="counter-right">
+            <span class="metrics-numbers">{{ metricsStore.time }}</span>
+            <span class="m-space"> s</span>
+          </span>
+        </div>
+        <div class="metrics-div">
+          <span class="counter-left">Average: </span>
+          <span class="counter-right">
+            <span class="metrics-numbers">{{ metricsStore.average }}</span>
+            <span class="m-space"> s</span>
+          </span>
+        </div>
+        <div class="metrics-div">
+          <span class="counter-left">Counter: </span>
+          <span class="counter-right">
+            <span class="metrics-numbers">{{ metricsStore.answers }}</span>
+            <span class="m-space"> x</span>
+          </span>
+        </div>
+      </v-card>
       <br />
-      <div class="metrics-div">
-        <span class="counter-left">Average: </span>
-        <span class="counter-right">
-          <span class="metrics-numbers">{{ metricsStore.average }}</span>
-          <span class="m-space"> s</span>
-        </span>
-      </div>
-      <br />
-      <div class="metrics-div">
-        <span class="counter-left">Counter: </span>
-        <span class="counter-right">
-          <span class="metrics-numbers">{{ metricsStore.answers }}</span>
-          <span class="m-space"> x</span>
-        </span>
-      </div>
-      <br />
-      <br />
-      <div class="metrics-div">
-        <span class="counter-left">Char / Min: </span>
-        <span class="counter-right">
-          <span class="metrics-numbers">{{ metricsStore.cpm }}</span>
-          <span class="m-space"> x</span>
-        </span>
-      </div>
-      <div class="metrics-div">
-        <span class="counter-left">Words / Min: </span>
-        <span class="counter-right">
-          <span class="metrics-numbers">{{ metricsStore.wpm }}</span>
-          <span class="m-space"> x</span>
-        </span>
-      </div>
-      <br />
+      <v-card class="metrics-card" hover>
+        <div class="metrics-div">
+          <span class="counter-left">Char / Min: </span>
+          <span class="counter-right">
+            <span class="metrics-numbers">{{ metricsStore.cpm }}</span>
+            <span class="m-space"> x</span>
+          </span>
+        </div>
+        <div class="metrics-div">
+          <span class="counter-left">Words / Min: </span>
+          <span class="counter-right">
+            <span class="metrics-numbers">{{ metricsStore.wpm }}</span>
+            <span class="m-space"> x</span>
+          </span>
+        </div>
+        <br />
+      </v-card>
     </div>
   </v-sheet>
 </template>
@@ -60,8 +61,12 @@ const metricsStore = useMetricsStore();
 <style scoped>
 .metrics-tab {
   margin-left: 8%;
-  margin-right: 10%;
-  font-size: 20px;
+  margin-right: 8%;
+  font-size: 16px;
+}
+
+.metrics-card {
+  padding: 10px 16px 10px 16px;
 }
 
 .metrics-div {
@@ -71,7 +76,7 @@ const metricsStore = useMetricsStore();
 
 .counter-left {
   float: left;
-  margin-top: 6px;
+  margin-top: 8px;
 }
 
 .counter-right {
@@ -80,7 +85,7 @@ const metricsStore = useMetricsStore();
 
 .metrics-numbers {
   font-weight: bold;
-  font-size: 29px;
+  font-size: 27px;
 }
 
 .m-space {
