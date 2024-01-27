c43c9b53884a4ccb8e15bcd564cd37fdb2bde9d9
chnmk <a2molchanov@ya.ru>
2024-01-27 14:30:00 +0300

    style: add bottom margin to about page

--- pages/about.vue
+++ pages/about.vue
@@ -2,34 +2,36 @@
   <div>
     <h1 class="sheet-header">{{ $t("About") }}</h1>
     <br />
-    <v-card class="description-card">
-      <div class="about-text">
-        {{ $t("ALanguageLearningApp") }}
-        <a href="https://play.typeracer.com/">typeracer</a>
-        {{ $t("and") }}
-        <a href="https://www.keybr.com/">keybr</a>.
-        {{ $t("ProvidesATyping") }}
-        <a href="https://tatoeba.org/">tatoeba.org</a>.
-      </div>
-      <br />
-      <div class="about-text">
-        {{ $t("techused") }}
-
-        <ul class="tech-list">
-          <li><a href="https://vuejs.org/">Vue 3</a></li>
-          <li><a href="https://nuxt.com/">Nuxt 3</a></li>
-          <li><a href="https://pinia.vuejs.org/">Pinia</a></li>
-          <li><a href="https://vuetifyjs.com/">Vuetify</a></li>
-        </ul>
-      </div>
-      <br />
-      <div class="about-text">
-        {{ $t("sourcecode") }}
-        <a href="https://github.com/chnmk/typelearner">GitHub</a>.
+    <div class="about-contents">
+      <v-card class="description-card">
+        <div class="about-text">
+          {{ $t("ALanguageLearningApp") }}
+          <a href="https://play.typeracer.com/">typeracer</a>
+          {{ $t("and") }}
+          <a href="https://www.keybr.com/">keybr</a>.
+          {{ $t("ProvidesATyping") }}
+          <a href="https://tatoeba.org/">tatoeba.org</a>.
+        </div>
         <br />
-        {{ $t("sentsource") }} <a href="https://tatoeba.org/">Tatoeba</a>.
-      </div>
-    </v-card>
+        <div class="about-text">
+          {{ $t("techused") }}
+
+          <ul class="tech-list">
+            <li><a href="https://vuejs.org/">Vue 3</a></li>
+            <li><a href="https://nuxt.com/">Nuxt 3</a></li>
+            <li><a href="https://pinia.vuejs.org/">Pinia</a></li>
+            <li><a href="https://vuetifyjs.com/">Vuetify</a></li>
+          </ul>
+        </div>
+        <br />
+        <div class="about-text">
+          {{ $t("sourcecode") }}
+          <a href="https://github.com/chnmk/typelearner">GitHub</a>.
+          <br />
+          {{ $t("sentsource") }} <a href="https://tatoeba.org/">Tatoeba</a>.
+        </div>
+      </v-card>
+    </div>
   </div>
 </template>
 
@@ -46,8 +48,8 @@ h1 {
   font-size: 25px;
 }
 
-.about-text {
-  margin-left: 5%;
+.about-contents {
+  padding-bottom: 3%;
 }
 
 .description-card {
@@ -55,6 +57,10 @@ h1 {
   height: 60%;
   margin: 0% 20% 0% 20%;
   padding: 2%;
+}
+
+.about-text {
+  margin-left: 5%;
 }
 
 .tech-list {
